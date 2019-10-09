c09c99cf125f47fb1a3fb5256094d3dd335366be
Rickard Jonsson <rickaard@gmail.com>
2019-10-09 14:10:54 +0200

    Fetching content on window load

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,61 @@
 "use strict";
 
-// Adds the class 'box-shadow' to the header when scroll down from top
-// Removes it when scrolled all the way up again
-window.addEventListener('scroll', () => {
-    const headerEl = document.querySelector('.header_wrapper');
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        headerEl.classList.add('box-shadow');
-    } else {
-        headerEl.classList.remove('box-shadow')
-    }
-})
+// Declare element variables
+const outputEl = document.querySelector('#fetch_output');
 
-const showMenuBtn = document.querySelector('#showMenu');
-const closeMenuBtn = document.querySelector('.closeMenu')
-showMenuBtn.addEventListener('click', () => {
-    document.querySelector('.nav_wrapper').classList.add('show');
+// API URL
+const url = 'http://localhost/api/index.php/courses/';
+
+// Call fetch function when DOM is finnished loading the DOM tree
+window.addEventListener('DOMContentLoaded', fetchContent);
+
+
+// Fetch content (the courses)
+function fetchContent()  {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log('fetchContent',data)
+        let courses = data.map((item, index) => {
+            return `
+            <tr class="course_wrapper">
+                <td class="course-code">${item.course_code}</td>
+                <td class="course-name">${item.course_name}</td>
+                <td class="course-progress">${item.course_progress}</td>
+                <td class="course-syllabus"><a target="_blank" href="${item.course_syllabus}">Webblänk</a></td>
+                <td class="course-delete"><button id="${item.id}" class="btn btn-small btn-warning">X</button></td>
+            </tr>
+            `
+        }).join("");
+        outputEl.innerHTML += courses;
+    })
+    .catch(error => console.log(error))
+}
+
+
+
+
+
+// Post new course
+const addCourse = (event) => {
+    event.preventDefault();
+    console.log('poop2');
+}
+
+// Edit course
+
+// Delete course
+function deleteCourse(event) {
+    console.log('poop');
+}
+
+
+document.querySelectorAll('.course-delete').forEach(element => {
+    element.addEventListener('click', deleteCourse);
+    console.log(element);
+    deleteCourse()
 });
-closeMenuBtn.addEventListener('click', () => {
-    document.querySelector('.nav_wrapper').classList.remove('show');
-})
+
+
+// Event listeners
+document.querySelector('#submitBtn').addEventListener('click', addCourse);
