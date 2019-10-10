05e07ee3caee009bfaf0310184d8cfb2579ff947
Rickard Jonsson <rickaard@gmail.com>
2019-10-10 17:46:16 +0200

    Add full CRUD functionality and styling

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,7 @@ const url = 'http://localhost/api/index.php/courses/';
 window.addEventListener('DOMContentLoaded', fetchContent);
 // Event listeners
 formEl.addEventListener('submit', addCourse);
+editFormEl.addEventListener('submit', editCourse);
 
 addModalBtn.addEventListener('click', () => {
     addModalEl.style.display = 'block';
@@ -44,7 +45,6 @@ function fetchContent()  {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        console.log('fetchContent',data)
         let courses = data.map((item, index) => {
             return `
             <tr class="course_wrapper">
@@ -52,8 +52,8 @@ function fetchContent()  {
                 <td class="course-name">${item.course_name}</td>
                 <td class="course-progress">${item.course_progress}</td>
                 <td class="course-syllabus"><a target="_blank" href="${item.course_syllabus}">Webblänk</a></td>
-                <td class="course-delete"><button id="${item.id}" class="btn btn-small btn-warning" onClick="deleteCourse(${item.id})">X</button></td>
-                <td class="course-delete"><button class="btn btn-small btn-warning" onClick="editModal('${item.course_code}', '${item.course_name}', '${item.course_progress}', '${item.course_syllabus}', ${item.id})">E</button></td>
+                <td class="course-delete"><button id="${item.id}" class="btn btn-small btn-warning" onClick="deleteCourse(${item.id})"><i class="fas fa-trash-alt"></i></button></td>
+                <td class="course-delete"><button class="btn btn-small btn-warning" onClick="editModal('${item.course_code}', '${item.course_name}', '${item.course_progress}', '${item.course_syllabus}', ${item.id})"><i class="fas fa-edit"></i></button></td>
             </tr>
             `
         }).join("");
@@ -63,18 +63,30 @@ function fetchContent()  {
 }
 
 // Convert the argument to a JSON object
-function convertToJson(codeInput, nameInput, progressInput, syllabusInput) {
+function convertToJson(codeInput, nameInput, progressInput, syllabusInput, id = undefined) {
     let course_code = codeInput;
     let course_name = nameInput;
     let course_progress = progressInput;
     let course_syllabus = syllabusInput;
 
-    let jsonStr = JSON.stringify({
-        'code': course_code,
-        'name': course_name,
-        'progress': course_progress,
-        'syllabus': course_syllabus
-    });
+    let jsonStr;
+
+    if (id == undefined) {
+        jsonStr = JSON.stringify({
+            'code': course_code,
+            'name': course_name,
+            'progress': course_progress,
+            'syllabus': course_syllabus,
+        });
+    } else {
+        jsonStr = JSON.stringify({
+            'code': course_code,
+            'name': course_name,
+            'progress': course_progress,
+            'syllabus': course_syllabus,
+            'id': id
+        });
+    }
     return jsonStr;
 }
 
@@ -94,7 +106,6 @@ function addCourse (event) {
     })
     .then(resp => resp.json())
     .then(data => {
-        console.log(data); // SKRIV UT ATT KURS ÄR TILLAGD ---------------------- OBSBSBOSBSOBSOBSOBSOBSOBS FIXA
         fetchContent(); // Fetching all the courses again when a new course is added
         formEl.reset(); // Resets the input fields in the form
         addModalEl.style.display = 'none';
@@ -104,59 +115,54 @@ function addCourse (event) {
 
 // Open edit modal
 function editModal(code, name, progress, syllabus, id) {
-    // console.log(code, name, progress, syllabus, id);
-
+    // Fill the form with correct values
     document.querySelector('#edit-course-form > div > #course_code').value = code;
     document.querySelector('#edit-course-form > div > #course_name').value = name;
     document.querySelector('#edit-course-form > div > #course_progress').value = progress;
     document.querySelector('#edit-course-form > div > #course_syllabus').value = syllabus;
     document.querySelector('#edit-course-form > div > #course_id').value = id;
-
     editModalEl.style.display = 'block';
-    console.log(code, name, progress, syllabus, id);
 }
 
 
 // Edit course
-function editCourse(code) {
-    // event.preventDefault();
+function editCourse(event) {
+    event.preventDefault();
     
-    console.log(code);
-    
+    let jsonStr = convertToJson(document.querySelector('#edit-course-form > div > #course_code').value, document.querySelector('#edit-course-form > div > #course_name').value, document.querySelector('#edit-course-form > div > #course_progress').value, document.querySelector('#edit-course-form > div > #course_syllabus').value, document.querySelector('#edit-course-form > div > #course_id').value);
 
-    // fetch(url, {
-    //     method: 'PUT',
-    //     header: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: jsonStr
-    // })
-    // .then(resp => resp.json())
-    // .then(data => {
-    //     console.log(data);
-    //     fetchContent();
-    //     // reset form inputs
-    // })
+    console.log(jsonStr);
+
+    fetch(url, {
+        method: 'PUT',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: jsonStr
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
+        fetchContent();
+        editModalEl.style.display = 'none';
+    })
 }
 
 // Delete course
 function deleteCourse(id) {
-    console.log(id);
     fetch(url+id, {
         method: 'DELETE'
     })
     .then(resp => resp.json())
     .then(data => {
-        console.log(data);
         fetchContent(); // Fetching all the courses again when a course is removed 
     })
     .catch(err => console.log(err))
 }
 
-
+// Event listener on every delete-button, call the deleteCourse function on click
 document.querySelectorAll('.course-delete > button').forEach(element => {
     element.addEventListener('click', deleteCourse);
-    console.log(element);
     deleteCourse()
 });
 
